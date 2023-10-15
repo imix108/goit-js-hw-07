38e9885e0f3c44f4f398466e96b7e4fba9ddb92b
imix108 <136431946+imix108@users.noreply.github.com>
2023-10-15 19:56:14 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+import { galleryItems } from './gallery-items.js';
+
+const list = document.querySelector('.gallery');
+
+function renderList(images) {
+  const markup = images.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}).join ('');
+
+  list.innerHTML = markup;
+}
+
+renderList(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',  
+  captionDelay: 250, 
+});
 console.log(galleryItems);
